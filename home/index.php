9d61addd3f9f24ae637563306618c7b1a7a9483e
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-14 00:14:27 +0100

    update

--- home/index.php
+++ home/index.php
@@ -42,7 +42,7 @@ page_header('Home');
 <script>var CSRFtoken = '<?= csrf_gen() ?>'; var auto_init = false;</script>
 
 <script src="https://cdn.lucacastelnuovo.nl/general/js/materialize.js"></script>
-<script src="https://cdn.lucacastelnuovo.nl/instakilo.lucacastelnuovo.nl/js/app.22.js"></script>
+<script src="https://cdn.lucacastelnuovo.nl/instakilo.lucacastelnuovo.nl/js/app.23.js"></script>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
