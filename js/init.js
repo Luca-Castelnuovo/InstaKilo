8860472cf86e32a3081771cd21bc47f1983a33fc
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-13 17:30:05 +0100

    update

--- js/init.js
+++ js/init.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Enable SideBar
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
+    var sidenav = M.Sidenav.init(document.querySelectorAll('.sidenav'), {
         edge: 'right',
         draggable: 1
     });
 
     // Add character counter to inputs
     M.CharacterCounter.init(document.querySelectorAll('.counter'));
+
+    // Enable MaterialBox
+    var materialbox = M.Materialbox.init(document.querySelectorAll('.materialboxed'), {});
 });
