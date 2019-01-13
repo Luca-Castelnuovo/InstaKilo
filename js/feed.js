8349ae9fc16711abcc6bc96956b9451c77a18128
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-13 14:10:21 +0100

    up

--- js/feed.js
+++ js/feed.js
@@ -1,8 +1,21 @@
+function request(method, url) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) return;
+        return JSON.parse(xhr.responseText);
+    };
+
+    xhr.open(method, url);
+    xhr.send();
+}
+
+
+function render_posts(data) {
+    console.log('posts', data);
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
-
-    //query json
-    //interpret json
-
-    //print to #post_container
-
+    render_posts(request('GET', `/posts/actions/${CSRFtoken}/feed`));
 });
