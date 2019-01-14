0d168268fb44bff3134aea869ad136dbcd777616
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-14 10:29:08 +0100

    update

--- user/profile.php
+++ user/profile.php
@@ -1,9 +1,8 @@
 <?php
 
+// CLEAN URL: /u/USERNAME -> /user/profile.php?user_name=$1
+
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/init.php';
 loggedin();
 
-
-//rewrite
-
-// /u/USERNAME -> /user/profile.php?user_name=$1
+echo "<h1>{$_GET['user_name']}</h1>";
