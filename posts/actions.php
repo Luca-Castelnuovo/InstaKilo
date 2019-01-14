b7a80a35e1163ea2ddf640757486374d30bd2d23
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-14 10:58:45 +0100

    update

--- posts/actions.php
+++ posts/actions.php
@@ -123,7 +123,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
         break;
 
     case 'POST':
-        if (!csrf_val($_POST'CSRFtoken'], 'override')) {
+        if (!csrf_val($_POST['CSRFtoken'], 'override')) {
             response(false, 'csrf_error', ['CSRFtoken' => csrf_gen()]);
         }
 
