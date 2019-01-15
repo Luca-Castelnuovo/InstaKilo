d91d05cc3adcafec0867a8df513322c8d3bce662
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-15 16:28:54 +0100

    wip user profile

--- user/profile.php
+++ user/profile.php
@@ -5,4 +5,24 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/includes/init.php';
 loggedin();
 
-echo "<h1>{$_GET['user_name']}</h1>";
+$user_name = clean_data($_REQUEST['user_name']);
+
+$user = sql_select('users', 'user_id,profile_picture,bio,following,is_private', "user_name='{$user_name}'", true);
+
+if (empty($user['user_id'])) {
+    redirect('/home', 'User doesn\'t exist');
+}
+
+page_header($user_name);
+
+?>
+
+<div class="row">
+    <div class="col s12 center">
+        <pre>
+            <?= var_dump($user) ?>
+        </pre>
+    </div>
+</div>
+
+<?= page_footer(); ?>
