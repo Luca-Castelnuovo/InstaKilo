d066361cd56c9ddee47ec2223d50011996dce845
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-15 17:39:51 +0100

    update

--- user/profile.php
+++ user/profile.php
@@ -18,19 +18,57 @@ page_header($user_name);
 ?>
 
 <div class="row">
-    <div class="col m4 hide-on-small-only">
-        <img src="<?= $user['profile_picture'] ?>" onerror="this.src='https://github.com/identicons/<?= $user_name ?>.png'" class="circle">
+    <!-- Phone -->
+    <div class="hide-on-med-and-up center">
+        <div class="row">
+            <div class="col s12">
+                <div class="card-panel">
+                    <div class="row">
+                        <div class="col s12">
+                            <img src="<?= $user['profile_picture'] ?>" onerror="this.src='https://github.com/identicons/<?= $user_name ?>.png'" class="circle" width="200">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12">
+                            <h2><?= $user_name ?></h2>
+                            <p><?= $user['bio'] ?></p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s12">
+                            <a href="#!" class="col s12 waves-effect waves-light btn tooltipped blue accent-4" data-position="bottom" data-tooltip="Unfollow">Follow</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col s12 show-on-small center">
-        <img src="<?= $user['profile_picture'] ?>" onerror="this.src='https://github.com/identicons/<?= $user_name ?>.png'" class="circle">
+
+    <!-- Tablet and up -->
+    <div class="hide-on-small-only">
+        <div class="row">
+            <div class="col m12">
+                <div class="card-panel">
+                    <div class="row">
+                        <div class="col m4">
+                            <img src="<?= $user['profile_picture'] ?>" onerror="this.src='https://github.com/identicons/<?= $user_name ?>.png'" class="circle" width="200">
+                        </div>
+                        <div class="col m6">
+                            <h2><?= $user_name ?></h2>
+                            <p><?= $user['bio'] ?></p>
+                        </div>
+                        <div class="col m2">
+                            <a href="#!" class="waves-effect waves-light btn-small tooltipped blue accent-4" data-position="bottom" data-tooltip="Unfollow">Follow</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col s12 m6">
-        <h2><?= $user_name ?></h2>
-        <p><?= $user['bio'] ?></p>
-    </div>
-    <div class="col s12 m2">
-        <a href="#!" class="waves-effect waves-light btn-small tooltipped blue accent-4" data-position="bottom" data-tooltip="Unfollow">Follow</a>
-    </div>
+</div>
+
+<div class="row">
+    <!-- List posts -->
 </div>
 
 <?= page_footer(); ?>
