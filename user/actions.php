8351fe0e629b06cf5fb00562cbf80e9d20a461d2
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-22 17:33:56 +0100

    update

--- user/actions.php
+++ user/actions.php
@@ -123,9 +123,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
                 foreach ($user_is_following as $following) {
                     $is_following = in_array($following, $visitor_followings) ? true : false;
-                    $is_user_self = ($_SESSION['id'] == $user['user_id']) ? true : false;
-
-                    response(false, '', ['session' => $_SESSION['id'], 'db' => $user['user_id']]);
+                    $is_user_self = ($_SESSION['id'] == $following) ? true : false;
 
                     $user_following = sql_select('users', 'user_name,profile_picture', "user_id='{$following}'", true);
 
