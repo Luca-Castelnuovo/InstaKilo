c55d368b9c310c92a305d4f121d3029400ffdf20
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-22 20:21:06 +0100

    updt

--- includes/template.php
+++ includes/template.php
@@ -63,7 +63,7 @@ function page_footer($extra = null)
         </div>
         {$extra}
         <script src="https://cdn.lucacastelnuovo.nl/general/js/materialize.js"></script>
-        <script src="https://cdn.lucacastelnuovo.nl/instakilo.lucacastelnuovo.nl/js/app.124.js"></script>
+        <script src="https://cdn.lucacastelnuovo.nl/instakilo.lucacastelnuovo.nl/js/app.125.js"></script>
 HTML;
     alert_display();
     echo <<<HTML
