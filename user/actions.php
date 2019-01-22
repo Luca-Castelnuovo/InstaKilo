5a66008ad2a60c535f295476273ac6b20d9dd24a
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-22 16:40:43 +0100

    update

--- user/actions.php
+++ user/actions.php
@@ -138,7 +138,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
 
             case 'feed':
                 $post_owner = sql_select('users', 'user_id', "user_name='{$username}'", true);
-                $posts = sql_select('posts', 'id,img_url,caption,allow_comments,comments,likes,liked_by,created,user_id', "user_id='{$post_owner['user_id']}' ORDER BY created DESC", false);
+                $posts = sql_select('posts', 'id,img_url,caption,likes,liked_by,user_id', "user_id='{$post_owner['user_id']}' ORDER BY created DESC", false);
 
                 if ($posts->num_rows != 0) {
                     $posts_item = [];
@@ -154,52 +154,17 @@ switch ($_SERVER['REQUEST_METHOD']) {
                             $user_is_owner_post = false;
                         }
 
-                        if ($post['allow_comments']) {
-                            $comments_item = [];
-                            foreach (json_decode($post['comments'], true) as $comment) {
-                                $owner_comments = sql_select('users', 'id,user_name,profile_picture', "user_id='{$comment['user_id']}'", true);
-
-                                if ($comment['user_id'] == $_SESSION['id']) {
-                                    $user_is_owner_comment = true;
-                                } else {
-                                    $user_is_owner_comment = false;
-                                }
-
-                                $comment_item = [
-                                    'id' => $comment['id'],
-                                    'username' => $owner_comments['user_name'],
-                                    'profile_picture' => $owner_comments['profile_picture'],
-                                    'body' => $comment['body'],
-                                    'user_is_owner' => $user_is_owner_comment
-                                ];
-
-                                array_push($comments_item, $comment_item);
-                            }
-
-                            $post_item = [
-                                'id' => $post['id'],
-                                'user_is_owner' => $user_is_owner_post,
-                                'username' => $owner['user_name'],
-                                'img_url' => $post['img_url'],
-                                'caption' => $post['caption'],
-                                'likes' => $post['likes'],
-                                'liked' => $liked,
-                                'comments_allowed' => true,
-                                'comments' => $comments_item,
-                            ];
-                        } else {
-                            $post_item = [
-                                'id' => $post['id'],
-                                'user_is_owner' => $user_is_owner_post,
-                                'username' => $owner['user_name'],
-                                'img_url' => $post['img_url'],
-                                'caption' => $post['caption'],
-                                'likes' => $post['likes'],
-                                'liked' => $liked,
-                                'comments_allowed' => false,
-                                'comments' => null
-                            ];
-                        }
+                        $post_item = [
+                            'id' => $post['id'],
+                            'user_is_owner' => $user_is_owner_post,
+                            'username' => $owner['user_name'],
+                            'img_url' => $post['img_url'],
+                            'caption' => $post['caption'],
+                            'likes' => $post['likes'],
+                            'liked' => $liked,
+                            'comments_allowed' => false,
+                            'comments' => null
+                        ];
 
                         array_push($posts_item, $post_item);
                     }
